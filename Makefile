655602f7d042d846df7926ef1994127e4b4e5a30
otohan <o.f.hanzlik@gmail.com>
2025-06-16 20:17:33 +1200

    version 0.1.0

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	g++ -o M3UCreator main.cpp
+
+clear:
+	rm -f test M3UCreator M3U_Creator.M3U
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<filesystem>
+#include<fstream>
+
+int main(){
+
+    std::string directory = std::filesystem::current_path();
+    std::string directory_name = std::filesystem::current_path().filename();
+
+    if(std::filesystem::exists(directory) && std::filesystem::is_directory(directory)){
+
+        //create/overwrite .M3U file.
+        std::ofstream FILE(directory_name + ".M3U");
+        
+        //list files into the .M3U file.
+        for(const auto& track : std::filesystem::directory_iterator(directory)){
+            
+            //for .mp3 files.
+            if(track.path().extension() == ".mp3"){
+                FILE << track.path().filename().string() << std::endl;
+            
+            //for .flac files.
+            }else if(track.path().extension() == ".flac"){
+                FILE << track.path().filename().string() << std::endl;
+            }
+        }
+
+    }else{
+        std::cerr << "M3U_Creator Cannot Work in the Current Directory.\n";
+    }
+
+    return 0;
+}

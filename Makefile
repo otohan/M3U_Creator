04edf1cb519ffb1eddb1d26eef0b1c6c84ef3faf
otohan <o.f.hanzlik@gmail.com>
2025-06-17 17:18:44 +1200

    version 0.1.1

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-build:
+build_l:
 	g++ -o M3UCreator main.cpp
 
+build_w:
+	g++ -o M3UCreator.exe main.cpp
+
 clear:
-	rm -f test M3UCreator M3U_Creator.M3U
+	rm -f test M3UCreator M3UCreator.exe M3U_Creator.M3U
--- main.cpp
+++ main.cpp
@@ -14,13 +14,13 @@ int main(){
         
         //list files into the .M3U file.
         for(const auto& track : std::filesystem::directory_iterator(directory)){
-            
+            std::string file_extension = track.path().extension();
             //for .mp3 files.
-            if(track.path().extension() == ".mp3"){
+            if(file_extension == ".mp3"){
                 FILE << track.path().filename().string() << std::endl;
             
             //for .flac files.
-            }else if(track.path().extension() == ".flac"){
+            }else if(file_extension == ".flac"){
                 FILE << track.path().filename().string() << std::endl;
             }
         }
